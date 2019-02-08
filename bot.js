ba44b7cce25bf15e757439cf0153fa28c4b1e9ed
TorpeDoSyS <45240133+TorpeDoSyS@users.noreply.github.com>
2019-02-08 19:04:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ msg.delete();
 //////////////////////////////
 client.on("ready", () => { // كود رينبو
   function lol() {
-    client.guilds.get('511582404113072138').roles.find("name", "Rainbow").setColor("RANDOM");
+    client.guilds.get('506916847791243273').roles.find("name", "Rainbow").setColor("RANDOM");
   };
   setInterval(lol, 2000);
 });
